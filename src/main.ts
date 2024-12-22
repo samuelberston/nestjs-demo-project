33bc8816131d8a7c27b72707734c633ec9ab935f
Samuel Berston <samuelberston@gmail.com>
2024-12-21 22:47:56 -0800

    use microservice package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 3000,
+        retryAttempts: 3,
+        retryDelay: 1000,
+        // Todo - add tlsOptions for secure connection
+      },
+    },
+  );
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen();
 }
 bootstrap();
